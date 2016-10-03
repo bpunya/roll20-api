f5e0b4618f61a08b210ded0fba1e27c69e99553e
bpunya <bodin.punyaprateep@gmail.com>
2016-10-03 00:55:58 -0400

    Update CombatMovement.js
    Final touches on CSS and some other oddities.

--- BackwaterCampaign/CombatMovement.js
+++ BackwaterCampaign/CombatMovement.js
@@ -25,9 +25,12 @@ var CombatMovement = CombatMovement || (function(){
     */
 
     var
-    version = "1.0",
-    lastUpdate = 1475429873,
+    version = '1.0',
+    lastUpdate = 1475470442,
+
+    // Update the following line to your character sheet attribute for movement speed
     movementattribute = 'speed',
+
     turnorder = {},
     turncounter = 1,
     initialtoken = false,
@@ -68,14 +71,19 @@ var CombatMovement = CombatMovement || (function(){
             turncounter++;
             printToChat({'who':'gm'}, `Round ${turncounter} has started.`)
         }
-        turnorder[currentTokenID][0] = turnorder[currentTokenID][1];
+        if(turnorder.hasOwnProperty(currentTokenID)) {
+            turnorder[currentTokenID][0] = turnorder[currentTokenID][1];
+        }
     },
 
     changeOptions = function(msg, option) {
         var actionTaken = false;
         switch(option) {
             case 'start':
-            if(!s.active && Campaign().get('initiativepage')) {
+            if(!s.active && Campaign().get('initiativepage') && !(initialtoken == false) ) {
+                s.active = true;
+                actionTaken = 'is now ACTIVE';
+            } else {
                 s.active = true;
                 freezeTurnOrder();
                 actionTaken = `is now ACTIVE. Round ${turncounter} has begun.`;
@@ -103,7 +111,7 @@ var CombatMovement = CombatMovement || (function(){
             break;
 
             case 'reset':
-            if(s.active) {
+            if(!(initialtoken == false)) {
                 initialtoken = false;
                 freezeTurnOrder();
                 actionTaken = 'has reset the stored turnorder'
@@ -163,33 +171,37 @@ var CombatMovement = CombatMovement || (function(){
             case '!CombatMovement':
             case '!CM':
 
-                if(msg.selected && args.length == 1) {
-                    if(!s.active) { return; }
+                if(msg.selected) {
+                    if(!s.active) {
+                        printToChat(msg, 'Please deselect all tokens and '+
+                        'start the script before attempting to add dash movement');
+                        return;
+                    }
                     var selectedtokenID,
                     selectedtokenNameArray = [];
 
                     for(i=0; i < msg.selected.length; i++) {
                         selectedtokenID = msg.selected[i]._id;
                         if(turnorder[selectedtokenID]) {
-                            turnorder[selectedtokenID][0] += turnorder[selectedtokenID][1]
-                            selectedtokenNameArray.push(getObj('graphic', selectedtokenID).get('name'))
+                            turnorder[selectedtokenID][0] += turnorder[selectedtokenID][1];
+                            selectedtokenNameArray.push(getObj('graphic', selectedtokenID).get('name'));
                         }
                     }
-                    output = `You have given dash movement to ${selectedtokenNameArray.join(', ')}`
+                    output = `You have given extra dash movement to ${selectedtokenNameArray.join(', ')}`;
                     printToChat(msg, output);
                     return;
                 }
 
                 switch(args[1]) {
-                    case '--start':
+                    case 'start':
                     changeOptions(msg, 'start')
                     break;
 
-                    case '--pause':
+                    case 'pause':
                     changeOptions(msg, 'pause')
                     break;
 
-                    case '--toggle':
+                    case 'toggle':
 
                         switch(args[2]) {
                             case 'auto-reset':
@@ -202,15 +214,15 @@ var CombatMovement = CombatMovement || (function(){
                         }
                     break;
 
-                    case '--stop':
+                    case 'stop':
                     changeOptions(msg, 'stop');
                     break;
 
-                    case '--reset':
+                    case 'reset':
                     changeOptions(msg, 'reset')
                     break;
 
-                    case '--help':
+                    case 'help':
                     showHelp(msg)
                     break;
 
@@ -247,22 +259,35 @@ var CombatMovement = CombatMovement || (function(){
 
     showHelp = function(msg) {
         var
-        currentState = s.active ? 'ACTIVE' : 'INACTIVE',
+        currentState = s.active ? 'RUNNING' : 'OFFLINE',
+        currentStateColour = s.active ? '#57961B' : '#991616',
         currentAutoResetState = s.autoreset ? ' automatically stop ' : ' continue to run ',
         helpContent = Chat_Formatting_START+
-                '<h3>Combat Movement help</h3><br>'+
-                '<strong>Available Options:</strong><br>'+
-                '--start <i>// Begins tracking token movement.</i><br>'+
-                '--pause <i>// Disables the script temporarily.</i><br>'+
-                '--toggle <i>// Toggles the current state.</i><br>'+
-                '--toggle auto-reset <i>// Controls whether the script will stop after the initiative window is closed.</i><br>'+
-                '--reset <i>// Resets all tracking (Use after a fight)</i><br>'+
-                '--stop <i>// Completely clears all tracked data and stops the script</i><br>'+
-                '<br>The script is currently <b>'+
+                '<div style="background-color: #282828;">'+
+                '<h3 style="color: #FFFFFF; text-align: center;">Combat Movement help</h3><br></div>'+
+                '<br>'+
+                '<strong>!CM or !combatmovement or !CombatMovement</strong><br>'+
+                '<i>// If units are selected, grant them double movement. Otherwise open this help box.</i><br><br>'+
+                '<b>start</b><br>'+
+                '<i>// Begins tracking token movement.</i><br>'+
+                '<b>pause</b><br>'+
+                '<i>// Disables the script temporarily.</i><br>'+
+                '<b>toggle</b><br>'+
+                '<i>// Toggles the current state.</i><br>'+
+                '<b>toggle auto-reset</b><br>'+
+                '<i>// Controls whether the script will stop after the initiative window is closed.</i><br>'+
+                '<b>reset</b><br>'+
+                '<i>// Resets all tracking (Use after a fight)</i><br>'+
+                '<b>stop</b><br>'+
+                '<i>// Completely clears all tracked data and stops the script</i><br><br>'+
+                'The script is currently <b><span style="color: #FFFFFF; background-color:'+
+                currentStateColour+
+                '">'+
                 currentState+
-                '</b>, and will'+
+                '</b></span>, and will'+
                 currentAutoResetState+
                 'when the turn order window is closed.'+
+                '</div>'+
                 Chat_Formatting_END;
         printToChat(msg, helpContent);
     };
@@ -278,38 +303,77 @@ var CombatMovement = CombatMovement || (function(){
         }
 
         // Get page properties
-        var movementX, movementY, higher, lower, totalmovement,
+        var movementX, movementY, lastmove, evencoords, oddcoords,
         currentPage = getObj('page', obj.get('_pageid')),
-        currentPageDiagonal = currentPage.get('diagonaltype'),
-        currentPageScaleNumber = currentPage.get('scale_number'),
+        currentPageDiagonalType = currentPage.get('diagonaltype'),
+        currentPageScale = currentPage.get('scale_number'),
+        currentPageGridSize = currentPage.get('snapping_increment'),
         currentPageGridType = currentPage.get('grid_type');
+        totaldistance = 0;
 
         // If the page is weird, just don't bother
         if( !(currentPageGridType == 'square')
             ) { return; }
 
-        // Check change in position
-        movementX = Math.abs((obj.get('left') - prev['left']))*currentPageScaleNumber/70;
-        movementY = Math.abs((obj.get('top') - prev['top']))*currentPageScaleNumber/70;
+        // Get movement coordinates and check if it was a simple move or waypoint
+        rawlastmove = obj.get('lastmove').split(',')
+        evenstrcoords = rawlastmove.filter(function(value, index) { return parseInt(index) % 2 == 0; });
+        evenintcoords = _.map(evenstrcoords, function(value) {
+                        return parseInt(value)
+                        });
+        oddstrcoords = rawlastmove.filter(function(value, index) { return parseInt(index) % 2 == 1; });
+        oddintcoords = _.map(oddstrcoords, function(value) {
+                        return parseInt(value)
+                        });
+        lastmove = _.zip(evenintcoords, oddintcoords);
 
-        if(currentPageDiagonal == 'foure') {
-            totalmovement = movementX > movementY ? movementX : movementY;
-        } else if(currentPageDiagonal == 'threefive') {
-            higher = movementX > movementY ? movementX : movementY;
-            lower = movementX < movementY ? movementX : movementY;
-            totalmovement = higher + (Math.floor(lower/10)*currentPageScaleNumber);
-        } else if(currentPageDiagonal == 'pythagorean') {
-            totalmovement = Math.sqrt(movementX^2 + movementY^2)
-        } else if(currentPageDiagonal == 'manhattan') {
-            totalmovement = movementX + movementY;
+        // We have movement coordinates, now to check if the player moved simply or used waypoints.
+        lastcoords = _.last(lastmove);
+        if(obj.get('left') != lastcoords[0] || obj.get('top') != lastcoords[1]) {
+            movementX = Math.abs(obj.get('left') - lastcoords[0])*currentPageScale*currentPageGridSize/70;
+            movementY = Math.abs(obj.get('top') - lastcoords[1])*currentPageScale*currentPageGridSize/70;
+            totaldistance += determineDistanceMoved(movementX, movementY, currentPageDiagonalType, currentPageScale);
         }
-
+        //FOR EACH JUMP, CALCULATE DISTANCE AND ADD IT TO A TOTAL.
+        for(i=0; i + 1 < lastmove.length; i++) {
+            movementX = Math.abs(lastmove[i][0] - lastmove[i+1][0])*currentPageScale*currentPageGridSize/70;
+            movementY = Math.abs(lastmove[i][1] - lastmove[i+1][1])*currentPageScale*currentPageGridSize/70;
+            totaldistance += determineDistanceMoved(movementX, movementY, currentPageDiagonalType, currentPageScale);
+        }
         // Can we move this distance? If no, stop.
-        if(turnorder[obj.id][0] < totalmovement) {
+        if(turnorder[obj.id][0] < totaldistance) {
             obj.set({left: prev.left, top: prev.top, rotation: prev.rotation});
         } else {
-            turnorder[obj.id][0] = turnorder[obj.id][0] - totalmovement;
+            turnorder[obj.id][0] = turnorder[obj.id][0] - totaldistance;
         }
+    },
+
+    determineDistanceMoved = function(x, y, diagonaltype, scale) {
+        var distanceMoved;
+        if(diagonaltype == 'foure')            { distanceMoved = get4eDistance(x, y, scale); }
+        else if(diagonaltype == 'threefive')   { distanceMoved = getThreeFiveDistance(x, y, scale); }
+        else if(diagonaltype == 'pythagorean') { distanceMoved = getEuclideanDistance(x, y, scale); }
+        else if(diagonaltype == 'manhattan')   { distanceMoved = getManhattanDistance(x, y, scale); }
+        return distanceMoved;
+    },
+
+    get4eDistance = function(x, y, scale) {
+        var totalmovement = x > y ? x : y;
+        return totalmovement;
+    },
+    getThreeFiveDistance = function(x, y, scale) {
+        var higher = x > y ? x : y,
+        lower = x < y ? x : y,
+        totalmovement = higher + (Math.floor(lower/10)*scale);
+        return totalmovement;
+    },
+    getEuclideanDistance = function(x, y, scale) {
+        var totalmovement = Math.sqrt(x^2 + y^2)
+        return totalmovement;
+    },
+    getManhattanDistance = function(x, y, scale) {
+        var totalmovement = x + y;
+        return totalmovement
     },
 
     registerEventHandlers = function(){
@@ -325,10 +389,8 @@ var CombatMovement = CombatMovement || (function(){
     };
 }());
 
-
 on('ready', function(){
     'use strict'
     CombatMovement.CheckVersion()
     CombatMovement.RegisterEventHandlers()
-    s.active = false;
 });
